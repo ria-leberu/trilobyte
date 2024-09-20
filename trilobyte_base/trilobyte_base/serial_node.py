33342fd66f5d89021ed595a8924349bcd000ca97
ria-leberu <jean@riabuildsthefuture.com>
2024-09-19 20:24:06 -0400

    Implemented Twist to serial comm

--- trilobyte_base/trilobyte_base/serial_node.py
+++ trilobyte_base/trilobyte_base/serial_node.py
@@ -10,6 +10,7 @@ WHEEL_RADIUS = 0.030
 WHEEL_SEPARATION = 0.24
 VEL_TO_PWM = 278
 PULSES_PER_REVOLUTION = 138
+PULSES_PER_METER = 732
 
 
 
@@ -22,35 +23,49 @@ class SerialNode(Node):
         self.serial_port = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=0.5)
         self.get_logger().info("serial_node starting . . . ")
     
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.linear.x)
-        # self.serial_port.write(bytes(msg.data, 'utf-8'))
-        self.twist_to_pwm(msg.linear.x, msg.angular.z)
-        time.sleep(0.01)
-        readLine = self.serial_port.readline()
-        stringLine = readLine.decode("utf-8")
-        print(stringLine)
-        # self.serial_port.close()
-    
-    def twist_to_pwm(self, linear_x, angular_z):
+    def listener_callback(self, msg) -> None:
 
-        pwm_left, pwm_right = 0, 0
+        serial_msg = self.twist_to_pwm(msg.linear.x, msg.angular.z)
+        self.serial_port.write(bytes(serial_msg, 'utf-8'))
+        self.get_logger().info(serial_msg)
+
+        time.sleep(0.01)
+        # readLine = self.serial_port.readline()
+        # stringLine = readLine.decode("utf-8")
+        # print(stringLine)
+    
+    def twist_to_pwm(self, linear_x, angular_z) -> str:
 
         speed_angle = angular_z
         speed_linear = linear_x
 
         wheel_left = (speed_linear/WHEEL_RADIUS) - ((speed_angle * WHEEL_SEPARATION)/(2.0 * WHEEL_RADIUS))
         wheel_right = (speed_linear/WHEEL_RADIUS) + ((speed_angle * WHEEL_SEPARATION)/(2.0 * WHEEL_RADIUS))
-        pwm_left = wheel_left * VEL_TO_PWM
-        pwm_right = wheel_right * VEL_TO_PWM
+        pwm_left = int(wheel_left * VEL_TO_PWM)
+        pwm_right = int(wheel_right * VEL_TO_PWM)
 
-        # TODO Account for negative PWM for shifting wheel direction and make appropriate calculations
+        # negative velocities are converted to new serial format
+        direction_left_wheel = 'f'
+        direction_right_wheel = 'f'
+
+        if pwm_left < 0:
+            pwm_left = pwm_left * (-1)
+            direction_left_wheel = 'b'
+        if pwm_right < 0:
+            pwm_right = pwm_right * (-1)
+            direction_right_wheel = 'b'
+
+        # Limit PWM output 
         if pwm_left > 200:
             pwm_left = 200
         if pwm_right > 200:
             pwm_right = 200
+        
+        # Pad leading zeroes for string 
+        string_left_wheel = str(pwm_left).zfill(3)
+        string_right_wheel = str(pwm_right).zfill(3)
 
-        return f"<"
+        return f"<{direction_left_wheel}{string_left_wheel}{direction_right_wheel}{string_right_wheel}>"
 
 
 
