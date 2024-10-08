1752b34a275b0b711b8f5f4e445269583b6fabe8
ria-leberu <jean@riabuildsthefuture.com>
2024-10-07 21:44:24 -0400

    ros2 control configuration

--- trilobyte_hardware_interface/launch/trilobyte_hardware_interface.launch.py
+++ trilobyte_hardware_interface/launch/trilobyte_hardware_interface.launch.py
@@ -65,12 +65,34 @@ def generate_launch_description():
     #     executable="joint_state_publisher_gui",
     #     condition=IfCondition(gui),
     # )
+
+    robot_controllers = PathJoinSubstitution(
+    [
+        FindPackageShare("trilobyte_hardware_interface"),
+        "config",
+        "controller_configuration.yaml",
+    ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+            ("/diffbot_base_controller/cmd_vel", "/cmd_vel"),
+        ],
+    )
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
     )
+
+
 
 
     nodes = [
