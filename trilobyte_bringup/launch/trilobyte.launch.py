4cb3b797824a8a6a652a47fb822711f1af0a5bd9
ria-leberu <jean@riabuildsthefuture.com>
2024-12-26 10:36:49 -0500

    added bringup file and folder

--- trilobyte_bringup/launch/trilobyte.launch.py
+++ trilobyte_bringup/launch/trilobyte.launch.py
@@ -1,12 +1,17 @@
-import launch
-import launch_ros.actions
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
+    included_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('trilobyte_hardware_interface'),
+                'launch',
+                'trilobyte_hardware_interface.launch.py'
+            )
         )
-    ])
+    )
+    return LaunchDescription([included_launch])
